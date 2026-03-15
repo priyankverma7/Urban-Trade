6aece7c2c7efde32af4a5fc05d511c252ae535a9
priyankverma7 <varmapriyank2211@gmail.com>
2026-03-15 11:03:07 +0530

    delete firebase integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CategoryPage from "./components/category/CategoryPage";
 import "./index.css";
@@ -8,22 +8,10 @@ import ProductDetails from "./components/products/ProductDetails";
 import Purchase from "./components/purchase/Purchase";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./utils/ProtectedRoute";
-
-import { useNotification } from "./hooks/useNotifications";
-import { requestPermissionAndGetToken } from "./firebase/messaging";
 import Profile from "./components/navbar/Profile";
 
 
 const App = () => {
-
-  useNotification();
-
-  useEffect(() => {
-    requestPermissionAndGetToken();
-  }, []);
-
-
-  
   return (
     <BrowserRouter>
       <Routes>
